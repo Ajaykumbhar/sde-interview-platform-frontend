419eaf8487cadd2e3f4548b0f6eceec6e5497c9f
Ajay kumbhar <ajaykumbhar891@gmail.com>
2026-09-19 21:49:48 +0530

    Fix of routes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,15 +2,32 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import HomePage from "../pages/home/HomePage";
 import LoginPage from "../pages/auth/LoginPage";
+import MainLayout from "../layouts/MainLayout";
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route
+          path="/"
+          element={
+            <MainLayout>
+              <HomePage />
+            </MainLayout>
+          }
+        />
+
+        <Route
+          path="/login"
+          element={
+            <MainLayout>
+              <LoginPage />
+            </MainLayout>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
 }
+
 export default AppRoutes;
